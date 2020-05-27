f23a7d05a43a5a67f7efc08f1bfc4433c6714ebb
Briano Goestiawan <briano@Brianos-MacBook-Pro.local>
2020-05-28 04:01:53 +1000

    Make page only renders once fonts are loaded

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import styled from 'styled-components';
-import Div100vh from 'react-div-100vh'
+import React from "react";
+import styled from "styled-components";
+import Div100vh from "react-div-100vh";
 
 export default () => {
   return (
     <App>
-      <Heading><b>briano</b></Heading>
+      <Heading>
+        <b>briano</b>
+      </Heading>
       <Links>
-        <Link href="https://github.com/bbriano" target="_blank"><b>Github</b></Link>
+        <Link href="https://github.com/bbriano" target="_blank">
+          <b>Github</b>
+        </Link>
       </Links>
     </App>
-  )
-}
+  );
+};
 
 const App = styled(Div100vh)`
   margin: auto;
@@ -20,37 +24,44 @@ const App = styled(Div100vh)`
   display: flex;
   flex-direction: column;
   justify-content: center;
-`
+`;
 
 const Heading = styled.span`
   font-family: Konstant Grotesk;
   font-size: 4em;
   animation: fadeIn 1s;
   @keyframes fadeIn {
-    from { opacity: 0 }
-    to { opacity: 1 }
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
   }
-`
+`;
 
 const Links = styled.div`
   display: flex;
   justify-content: flex-end;
-  background: var(--foreground);
-  transform: translateX(60%);
   animation: fromRight 1s 300ms forwards;
+  transform: translateX(60%);
   @keyframes fromRight {
-    from { transform: translateX(60%); }
-    to { transform: translateX(0); }
+    from {
+      transform: translateX(60%);
+    }
+    to {
+      transform: translateX(0);
+    }
   }
-`
+`;
 
 const Link = styled.a`
   font-family: Radio Grotesk;
   color: var(--red);
-  transition: 150ms;
+  transition: color 150ms;
   text-decoration: none;
   :hover {
     color: var(--magenta);
     text-decoration: underline;
   }
-`
+`;
