2dcce7a2b9ec86b287241221e1b6a70904032759
Briano Goestiawan <b@briano.io>
2020-12-03 16:02:44 +1100

    cubed

---
+++ src/App.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+    import Cube from "./Cube.svelte";
+</script>
+
+<svelte:head>
+    <title>briano</title>
+</svelte:head>
+
+<center>
+    <Cube />
+</center>
+
+<center>
+    <pre>
+***************************************
+
+
+<span
+            style="color:white">briano's page</span>
+
+<a
+            href="mailto:b@briano.io">mail:b@briano.io</a>    <a
+            href="https://github.com/bbriano">github:bbriano</a>
+***************************************
+    </pre>
+</center>
+
+<pre class="links">
+<br />
+    <a
+        href="https://notes.briano.io/cv.html">CV</a>
+
+    <a
+        href="https://notes.briano.io">notes</a>
+
+    <a
+        href="https://publioofficial.com">Publio</a>
+
+    <a
+        href="https://typings.gg">typings.gg</a>
+
+    <a
+        href="https://briano1905.com/gmk-wob-hiragana/">GMK WoB Hiragana</a>
+
+    <a
+        href="https://vce-notes.netlify.app">vce notes</a>
+
+    <a
+        href="https://briano1905.com/vector-3d">Vector 3D</a>
+
+    <a
+        href="https://briano1905.com/internet-usage">Internet Usage</a>
+
+    <a
+        href="https://briano1905.com/gmk-phantom/">GMK Phantom</a>
+</pre>
+
+<style>
+    pre {
+        font-size: 2rem;
+    }
+
+    center {
+        color: blueviolet;
+    }
+    center a {
+        color: blue;
+    }
+
+    .links {
+        max-width: 60rem;
+        margin: auto;
+    }
+    a {
+        color: limegreen;
+        text-decoration: none;
+    }
+    a:hover {
+        color: goldenrod;
+    }
+</style>
---
+++ src/Cube.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    import * as THREE from "three";
+    import { onMount } from "svelte";
+
+    let canvas: HTMLDivElement;
+    let camera: THREE.PerspectiveCamera;
+    let scene: THREE.Scene;
+    let renderer: THREE.Renderer;
+    let geometry: THREE.Geometry;
+    let material: THREE.Material;
+    let mesh: THREE.Mesh;
+
+    onMount(() => {
+        init();
+        animate();
+    });
+
+    function init() {
+        camera = new THREE.PerspectiveCamera(
+            70,
+            canvas.clientWidth / canvas.clientHeight,
+            0.01,
+            10
+        );
+        camera.position.z = 1;
+        scene = new THREE.Scene();
+        geometry = new THREE.BoxGeometry(0.6, 0.6, 0.6);
+        material = new THREE.MeshNormalMaterial();
+        mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+        canvas.appendChild(renderer.domElement);
+
+        window.addEventListener("resize", () => {
+            camera.updateProjectionMatrix();
+        });
+    }
+
+    function animate() {
+        requestAnimationFrame(animate);
+        mesh.rotation.x += 0.01;
+        mesh.rotation.y += 0.02;
+        renderer.render(scene, camera);
+    }
+</script>
+
+<div class="canvas" bind:this={canvas} />
+
+<style>
+    .canvas {
+        width: 100%;
+        height: 14rem;
+    }
+</style>
